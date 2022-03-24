97122cd1ebc80c6880e5348b6c7836d7796217f8
Casey Harding <casey.r.harding@gmail.com>
2022-03-24 12:35:44 -0400

    Through to initial friend styling.

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+
+const StyledFriend = styled.div`
+  width: 60%;
+  display: flex;
+  justify-content: space-between;
+  padding: 8px;
+  border-bottom: 2px solid white;
+`
 
 export default function Friend({ info, action }) {
   return (
-    <div className='friend'>
+    <StyledFriend className='friend'>
       {info.name}
       <button onClick={() => action(info.id)}>
         See details
       </button>
-    </div>
+    </StyledFriend>
   )
 }
